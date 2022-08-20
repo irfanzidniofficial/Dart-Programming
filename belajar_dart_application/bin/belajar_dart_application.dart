9c5ae6e810042b91bc666dd25782e3fdf0fc4ecc
Irfan Zidni <irfanzidniofficial@gmail.com>
2022-08-20 21:49:23 +0700

    compile app

---
+++ belajar_dart_application/bin/belajar_dart_application.dart
@@ -0,0 +1,8 @@
+import 'package:belajar_dart_library/hello.dart' as belajar;
+
+void main() {
+  print(belajar.sayHello("Irfan"));
+  var customer = belajar.Customer();
+  var product = belajar.Product();
+  var category = belajar.Category();
+}
